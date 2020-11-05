ce574b6c478e06e3bac980d6dfef132ca794812d
srika <srika@LAPTOP-81P5HPDV>
2020-11-05 15:30:00 +0530

    2

--- FileDownloadService/src/com/nt/FileDownload.java
+++ FileDownloadService/src/com/nt/FileDownload.java
@@ -9,18 +9,27 @@ import javax.ws.rs.core.Response.ResponseBuilder;
 
 @Path("/files")  
 public class FileDownload {  
-   private static final String FILE_PATH = "e:\\myfile.txt";  
+   private static final String FILE_PATH = "e:\\Resume.pdf"; //local file location
     
     @GET 
     @Path("/txt")  
-    @Produces("text/plain")  
+    @Produces("application/pdf")
+    
+//    @Produces("text/plain"): for downloading text file.
+//    @Produces("image/png"): for downloading png image file.
+//    @Produces("application/pdf"): for downloading PDF file.
+//    @Produces("application/vnd.ms-excel"): for downloading excel file.
+//    @Produces("application/msword"): for downloading ms word file.
+    
     public Response getFile() {  
     	System.out.println("hello");
-        File file = new File(FILE_PATH);  
+    	
+        File file = new File(FILE_PATH); //find the file location 
    
-        ResponseBuilder response = Response.ok((Object) file);  
-        response.header("Content-Disposition","attachment; filename=\"Srikanth_file.txt\"");  
-        return response.build();  
+        ResponseBuilder response = Response.ok((Object) file);   //file is here ref  type to convert object type
+        response.header("Content-Disposition","attachment; filename=\"Srikanth_file.pdf\"");  
+// response is reference.header("string any name","object type any name; save name->filename="\filename.pdf");
+        return response.build();  //return type Response 
    
     }  
  }  
